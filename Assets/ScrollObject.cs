685652c03871a6be26021f69c74ca5ac3372912b
Alex Kim <soulside44@gmail.com>
2019-11-20 16:28:21 +0900

    sprite scripting
    스프라이트를 무한히 반복적으로 나타나도록 구현,
    스프라이트 속도를 다르게 구현

---
+++ Assets/ScrollObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float startPosition;
+    [SerializeField] float endPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-speed*Time.deltaTime, 0, 0);
+        if(transform.position.x<=endPosition)
+        {
+            transform.Translate(-(endPosition-startPosition),0,0);
+        }
+    }
+}
